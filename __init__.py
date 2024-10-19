ffc9a93922e08462ce1664b2a76f2e29a81fe792
Sharan Suri <sharansuri-8@hotmail.com>
2024-10-19 18:35:09 +0530

    feat: added options for random order and limited length

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 # Imports:
 import utils.fetchRestraunt as fetchRestraunt
 from flask import Flask, jsonify, request
+from random import shuffle
 
 app = Flask(__name__)
 
@@ -9,6 +10,15 @@ app = Flask(__name__)
 def restraunt():
   data = request.json
   url = data.get('url') if data else None
+  length = data.get('length') if data else None
+  random_order = data.get('random') if data else False
+
+  if url and length and random_order:
+    itemarr = fetchRestraunt.getRestraunt(url)
+    shuffle(itemarr)
+    return jsonify(itemarr[0:length])
+  if url and length:
+    return jsonify(fetchRestraunt.getRestraunt(url)[0:length])
   if url:
     return jsonify(fetchRestraunt.getRestraunt(url))
   return jsonify({'error': 'Please provide a URL'})
