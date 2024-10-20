b49c50fea432c6549b6dd36e3e3e1fe33e6831f2
Sharan Suri <sharansuri-8@hotmail.com>
2024-10-21 03:04:39 +0530

    feat: added dotenv and another route

--- __init__.py
+++ __init__.py
@@ -2,13 +2,29 @@
 import utils.fetchRestraunt as fetchRestraunt
 from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 from random import shuffle
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 # Functions:
+@app.route('/')
+def home():
+  response = make_response('hello')
+  response.status_code = 200
+  return response
+
 @app.route('/api/v1/restraunt', methods=['POST'])
 def restraunt():
+  return jsonify({'error': 'W.I.P'})
+
+@app.route('/api/v1/restraunt/menu', methods=['POST'])
+def menu():
   data = request.json
   url = data.get('url') if data else None
   length = data.get('length') if data else None
@@ -21,10 +37,12 @@ def restraunt():
   if url and length:
     return jsonify(fetchRestraunt.getRestraunt(url)[0:length])
   if url:
-    return jsonify(fetchRestraunt.getRestraunt(url))
+    response = make_response(jsonify(fetchRestraunt.getRestraunt(url)))
+    response.status_code = 200
+    return response
   response = make_response(jsonify({'error': 'Please provide a URL'}))
   response.status_code = 400
   return response
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=os.getenv("SERVER_IP"), port=os.getenv("PORT"), debug=True)
